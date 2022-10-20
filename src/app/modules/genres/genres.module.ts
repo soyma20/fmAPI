ff9c6523ca9427d9f99d05b5c014e6148f78044e
soyma20 <slaviksoyma2014@gmail.com>
2022-10-20 20:50:09 +0300

    feat: add genres modules

---
+++ src/app/modules/genres/genres.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {GenresComponent} from './components/genres/genres.component';
+import {GenresRoutingModule} from "./genres-routing.module";
+import {GenreService} from "./services/genre.service";
+import {GenresResolver} from "./services/genres.resolver";
+import {HttpModule} from "../http/http.module";
+import { GenreComponent } from './components/genre/genre.component';
+
+
+@NgModule({
+  declarations: [
+    GenresComponent,
+    GenreComponent,
+  ],
+  imports: [
+    CommonModule,
+    HttpModule,
+    GenresRoutingModule,
+  ],
+  providers:[
+    GenreService,
+    GenresResolver,
+  ]
+})
+export class GenresModule { }

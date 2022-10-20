bd8e0f74084566cec252af7a20b7c4d4f2cbd918
soyma20 <slaviksoyma2014@gmail.com>
2022-10-20 16:14:11 +0300

    feat: add urls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LikesComponent } from './components/likes/likes.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LikesComponent,
+    SearchInputComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/constants/token.ts
@@ -0,0 +1,1 @@
+export const token = '22e5dcb7293a23da484afeacce80c247'

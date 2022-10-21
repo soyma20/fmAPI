73729c604a226466d188039b5df12be2c189480a
soyma20 <slaviksoyma2014@gmail.com>
2022-10-21 21:34:37 +0300

    feat: add functions to search input

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {IAlbum} from "../../interfaces/IAlbum";
+import {DataService} from "../../services/data.service";
 
 @Component({
   selector: 'app-search-input',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-input.component.css']
 })
 export class SearchInputComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private dataService: DataService) {
   }
 
+  ngOnInit(): void {
+    this._createForm()
+  }
+
+  _createForm(): void {
+    this.form = new FormGroup({
+      input: new FormControl(),
+    })
+  }
+
+  search() {
+    this.dataService.storage = this.form.getRawValue().input
+  }
 }

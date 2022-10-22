6e19264b60d57e73188df68239aa74b23fff25c2
soyma20 <slaviksoyma2014@gmail.com>
2022-10-22 14:01:41 +0300

    refactor: change structure of proj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
-import {MainLayoutComponent} from "./layouts/main-layout/main-layout.component";
+import {AppComponent} from "./app.component";
 
 const routes:Routes=[
-  {path:'', component:MainLayoutComponent, children:[
+  {path:'', component:AppComponent, children:[
       {path:'', redirectTo:'genres', pathMatch:'full'},
       {path: 'genres', loadChildren:()=> import('./modules').then(value => value.GenresModule) },
       {path: 'albums/:genre', loadChildren:()=> import('./modules').then(value => value.AlbumsModule) },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LikesComponent } from './components/likes/likes.component';
-import { SearchInputComponent } from './components/search-input/search-input.component';
-import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
-import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    LikesComponent,
-    SearchInputComponent,
-    MainLayoutComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        ReactiveFormsModule
     ],
   providers: [],
   bootstrap: [AppComponent]

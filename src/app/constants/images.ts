077e01aa5ff402738705a8b8472ba615a666872a
soyma20 <slaviksoyma2014@gmail.com>
2022-12-24 17:54:38 +0200

    feat: add images links file

---
+++ src/app/constants/images.ts
@@ -0,0 +1,8 @@
+export const Images:string[] =[
+  '/assets/207c9708aabfc084aee235ab2c8c4ee9.png',
+  '/assets/645ef9545c4ac26bc61563ca7a2c0bf3.png',
+  '/assets/2fd6dca1dcd0da717d58d0a897689f7a.png',
+  '/assets/0326b3a1f32cd199238f9c0b8db3d51b.png',
+  '/assets/c0bde9b59cd4f35a03cf1e0c6db3284c.png',
+  '/assets/a60d9d9ae55226699420b52ab28d3ad0.png'
+]

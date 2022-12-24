44c7202d93bf3f7140f58120127d20847c3d54ae
soyma20 <slaviksoyma2014@gmail.com>
2022-12-24 17:55:40 +0200

    fix: remove token to env file

--- src/app/constants/token.ts
+++ src/app/constants/token.ts
@@ -1,1 +1,5 @@
-export const token = '22e5dcb7293a23da484afeacce80c247'
+import {environment} from "../../environments/environment";
+
+export const {token} = environment;
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   API: 'https://ws.audioscrobbler.com/2.0',
+  token: '22e5dcb7293a23da484afeacce80c247'
 };
 
